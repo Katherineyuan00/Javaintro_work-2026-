c5983362e18027e209412d3a0bf79c2af615f1bf
Katherine Yuan <katherineyuan00@gmail.com>
2026-03-11 20:04:29 -0400

    Add files via upload

---
+++ Katherineweek1.java
@@ -0,0 +1,26 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+String favfood = "pasta";
+int age = 16;
+String name = "Katherine";
+boolean bool = true;
+char firstnameletter = 'k';
+String school = "lajeunesse";
+
+        System.out.println(favfood);
+        System.out.println(school);
+        System.out.println(age);
+        System.out.println (name);
+        System.out.println(bool);
+        System.out.println(firstnameletter);
+float number1 = 7;
+float number2 = 2;
+
+System.out.println(number1/number2);
+
+    }
+
+}
+
